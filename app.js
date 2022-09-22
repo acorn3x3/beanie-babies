7f5c8b21e1c8975baefaa20537985aa758847c02
Andrew Cornejo <atcornejo3@gmail.com>
2022-09-22 16:41:27 -0700

    functionality isnt working. Beanie Babies are not displaying on page. Investigating but commiting for posterity

--- app.js
+++ app.js
@@ -1,15 +1,83 @@
 /* Imports */
-console.log('initial commit for dev');
+// > Part A: Import `getBeanieBabies` from fetch-utils.js
+// > Part B: Import `getastrosign` from fetch-utils.js
+//import { renderContinentOption, renderCountry } from './render-utils.js';
 
 /* Get DOM Elements */
 const notificationDisplay = document.getElementById('notification-display');
 const searchForm = document.getElementById('search-form');
-const astrosignSelect = document.getElementById('astrosign-select');
-const beaniebabyList = document.getElementById('beaniebaby-list');
+const astrosignSelect = document.getElementById('astroSign-select');
+const beanieBabyList = document.getElementById('beanie-list');
+
 /* State */
+let error = null;
+let count = 0;
+let astrosign = [];
+let beanieBabies = [];
 
 /* Events */
+window.addEventListener('load', async () => {
+    // > Part A: call findBeanieBabies (with no arguments)
+    findBeanieBabies();
+
+    // > Part B: await the call to get astrosign to get the response
+
+    // > Part B: Assign to state the:
+    //      - error,
+    //      - data (to the astrosign variable)
+
+    if (!error) {
+        displayContinentOptions();
+    }
+});
+
+async function findBeanieBabies(title, astrosign) {
+    // > Part A: Call the service function that gets the BeanieBabies
+
+    // > Part C: Add the title and astrosign arguments to getBeanieBabies
+
+    // > Part A: Assign to state the :
+    //      - error,
+    //      - data (to the BeanieBabies variable)
+
+    // > Part D: Assign to state the:
+    //      - count (of db records)
+
+    displayNotifications();
+    if (!error) {
+        displayBeanieBabies();
+    }
+}
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(searchForm);
+    // > Part C: Call findBeanieBabies with name and continent from formData
+});
 
 /* Display Functions */
+function displayBeanieBabies() {
+    countryList.innerHTML = '';
 
-// (don't forget to call any display functions you want to run on page load!)
+    for (const beanieBaby of beanieBabies) {
+        // > Part A: render and append to list
+    }
+}
+
+function displayNotifications() {
+    if (error) {
+        notificationDisplay.classList.add('error');
+        notificationDisplay.textContent = error.message;
+    } else {
+        notificationDisplay.classList.remove('error');
+        // > Part D: Display a message with
+        //      - how many items were returned in BeanieBabies array
+        //      - how many total matching BeanieBabies were in the db
+    }
+}
+
+function displayContinentOptions() {
+    for (const astrosign of astrosigns) {
+        // > Part B: render and append options to select
+    }
+}
---
+++ fetch-utils.js
@@ -0,0 +1,40 @@
+const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+// export async functions that fetch data
+
+export async function getBeanieBabies(name, astroSign) {
+    // > Part D: Add a second argument to `select(` to
+    // return an exact db count of matching records
+
+    // > Part A: Implement the client query from countries:
+    //   1. select all columns
+    //   2. order by country name
+    //   3. limit to 100 countries
+    let query = client
+        .from('beanie_babies')
+        .select('*', { count: 'exact' })
+        .order('releaseYear')
+        .limit(100);
+
+    if (name) {
+        query = query.ilike('title', `%${name}%`);
+        // > Part C: add query for name
+    }
+
+    if (astrosign) {
+        query = query.eq('astroSign', astroSign);
+        // > Part C: add query for continent
+    }
+    const response = await query;
+    return response;
+    // > Part A: `await` the query and return the response
+}
+
+export async function getAstrosigns() {
+    // > Part B: await client query from country_continents
+    // (select all columns) and return response
+}
